c2dc60cf9ff9f48fa6995987769cde1cb7438a02
zinhtrann <tranthevinh44464@gmail.com>
2024-10-17 00:41:02 +0700

    TranTheVinh

---
+++ GiaoDienAdmin/GiaoDienAdmin/Pages/DanhMuc.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GiaoDienAdmin.Pages
+{
+    public class DanhMucModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ GiaoDienAdmin/GiaoDienAdmin/Pages/DonHang.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GiaoDienAdmin.Pages
+{
+    public class DonHangModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
