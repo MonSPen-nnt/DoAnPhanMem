b211c6333515fac58710ad2761cb01876756a4c5
MonSPen-nnt <monpen.nnt@gmail.com>
2024-10-23 22:46:49 +0700

    Goi y San Pham theo danh muc cua sp trong chitiet

---
+++ GoiYSanPham/DAPMver1/DAPMver1/Models/ChiTietSanPhamViewModels.cs
@@ -0,0 +1,10 @@
+﻿using DAPMver1.Data;
+
+namespace DAPMver1.Models
+{
+    public class ChiTietSanPhamViewModels
+    {
+        public SanPham SanPham { get; set; }
+        public IEnumerable<SanPham> GoiYSanPhams { get; set; }
+    }
+}
