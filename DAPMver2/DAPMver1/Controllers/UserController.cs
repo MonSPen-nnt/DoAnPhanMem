3f8a26ec877c3ea6310d445f8445f7e4bc95c7fa
khaokhat77 <khaokhat77@gmail.com>
2024-10-24 08:46:12 +0700

    DanhGiaSanPham

---
+++ DAPMver2/DAPMver1/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using DAPMver1.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BanTrangSuc.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly DapmTrangv1Context db;
+        public IActionResult CreateUserDetails()
+        {
+              return View();
+            
+        }
+
+        public IActionResult EditUser()
+        {
+            return View();
+        }
+        public IActionResult UserDetails()
+        {
+            return View();
+        }
+    }
+}
