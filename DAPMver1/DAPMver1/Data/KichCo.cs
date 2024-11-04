05ea122c5b238d043d38511643e91e9430094ef6
MonSPen-nnt <monpen.nnt@gmail.com>
2024-11-04 22:56:29 +0700

    QuanLyThongTinCaNhan,QuenMatKhau

---
+++ DAPMver1/DAPMver1/Data/KichCo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAPMver1.Data;
+
+public partial class KichCo
+{
+    public int MaKichCo { get; set; }
+
+    public int? MaSanPham { get; set; }
+
+    public int SoKichCo { get; set; }
+
+    public int SoLuong { get; set; }
+
+    public virtual SanPham? MaSanPhamNavigation { get; set; }
+
+    public virtual ICollection<ChiTietGioHang> ChiTietGioHangs { get; set; } = new List<ChiTietGioHang>();
+
+}
